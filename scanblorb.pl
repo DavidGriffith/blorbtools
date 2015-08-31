3e5f7d9f3b4a268a2b0d9a4e840131d76b30cb07
David Griffith <dave@661.org>
2015-08-30 19:01:15 -0700

    Update scanblorb to Richard Poole's changes of 2004.

--- scanblorb.pl
+++ scanblorb.pl
@@ -1,18 +1,27 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 #  scanBlorb: a perl script for scanning Blorb files
-#  (c) Graham Nelson 1998
+#  (c) Graham Nelson 1998 (original script)
+#  (c) Richard Poole 2004 (modifications)
 
-#$input_filename = '$.Adventure.Blorb.blorbfile';
-$input_filename = $ARGV[0];
-chomp($input_filename);
+use strict;
+use Getopt::Long;
 
-$version = "scanBlorb 1.0";
+require 5.6.0;
 
-($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime(time);
+my $buffer;
+my $dump_images;
+my %images;
 
-$blorbdate = sprintf("%02d%02d%02d at %02d:%02d.%02d",
-                 $year, $month + 1, $mday, $hour, $min, $sec);
+GetOptions("dump-images" => \$dump_images);
+my $input_filename = $ARGV[0];
+
+my $version = "scanBlorb 2.0";
+
+my ($sec,$min,$hour,$mday,$month,$year) = (localtime(time))[0, 1, 2, 3, 4, 5];
+
+my $blorbdate = sprintf("%04d/%02d/%02d at %02d:%02d.%02d",
+                 $year + 1900, $month + 1, $mday, $hour, $min, $sec);
 
 print STDOUT "$version [executing on $blorbdate]\n\n";
 
@@ -21,79 +30,88 @@ binmode(BLORB);
 
 read BLORB, $buffer, 12;
 
-($w1, $w2, $w3) = unpack("NNN", $buffer);
+my ($groupid, $length, $type) = unpack("NNN", $buffer);
 
-if ($w1 != 0x464F524D) { print "Not a valid FORM file\n"; }
-if ($w3 != 0x49465253) { print "Doesn't carry the Blorb magic word\n"; }
+$groupid == 0x464F524D or die "Not a valid FORM file!\n";
+$type == 0x49465253 or die "Not a Blorb file!\n";
 
-printf STDOUT ("File length claimed to be %06x = $w2 bytes\n", $w2);
+print "File length is apparently $length bytes\n";
 
-$posn = 12;
+my ($size, $pos);
 
-while ($posn < $w2)
-{   # Read in a chunk
+for($pos = 12; $pos < $length; $pos += $size + ($size % 2) + 8) {
+	my $chunkdata;
 
-    $headlength = read BLORB, $buffer, 8;
+	read(BLORB, $buffer, 8) == 8
+		or die("Incomplete chunk header at $pos\n");
 
-    if ($headlength != 8)
-    {   printf STDOUT ("Error: incomplete chunk header at %06x\n", $posn);
-        $posn = $w2;        
-    }
-    else
-    {   ($junk, $size) = unpack("NN", $buffer);
-        $type = substr($buffer, 0, 4);
-        printf STDOUT ("%08x: %s chunk with %06x = %d bytes of data\n",
-            $posn, $type, $size, $size);
+	$size = (unpack("NN", $buffer))[1]; # second word of header
+	my $type = substr($buffer, 0, 4);
+	printf "%06x: $type chunk with $size bytes of data\n", $pos;
 
-        $chopflag = 0;
-        if ($size % 2 == 1) { $size = $size + 1; $chopflag = 1; }
+	read(BLORB, $chunkdata, $size) == $size
+		or die("Incomplete chunk at $pos\n");
+	if($size % 2) { read(BLORB, $buffer, 1); }
 
-        $bodylength = read BLORB, $chunkdata, $size;
-        if ($bodylength != $size)
-        {   printf STDOUT ("Error: incomplete chunk at %08x\n", $posn);
-            $posn = $w2;
-        }
-        else
-        {   $posn = $posn + $size + 8;
-            if ($chopflag == 1) { chop($chunkdata); }
+	# optional chunks
+	if ($type eq "(c) " or $type eq "AUTH" or $type eq "ANNO") {
+		print "$type: $chunkdata\n";
+	}
 
-            if ($type eq "(c) ")
-            {   print STDOUT "          $chunkdata\n";
-            }
-            if ($type eq "AUTH")
-            {   print STDOUT "          $chunkdata\n";
-            }
-            if ($type eq "ZCOD")
-            {   ($version, $ignore, $release) = unpack("CCn", $chunkdata);
-                $serialcode = substr($chunkdata, 0x12, 6);
-                print STDOUT
-                    "          $release.$serialcode (version $version)\n";
-            }
-            if ($type eq "IFhd")
-            {   
-                $release = unpack("n", substr($chunkdata,0,3));
-                $serialcode = substr($chunkdata, 2, 6);
-                print STDOUT
-                    "          $release.$serialcode\n";
-            }
-            if ($type eq "RelN")
-            {   $relnum = unpack("n", $chunkdata);
-                print STDOUT "          Release number $relnum\n";
-            }
-            if ($type eq "RIdx")
-            {   print STDOUT "          Resources index:\n";
+	# zcode executable: look into its magic insides
+	if ($type eq "ZCOD") {
+		my ($version, $release) = (unpack("CCn", $chunkdata))[0,2];
+	    my $serialcode = substr($chunkdata, 0x12, 6);
+	    print "\t$release.$serialcode (version $version)\n";
+	}
 
-                $numres = unpack("N", $chunkdata);
-                for ($x = 0; $x < $numres; $x = $x + 1)
-                {   $indexentry = substr($chunkdata, 4 + $x*12, 12);
-                    $usagestring = substr($indexentry, 0, 4);
-                    ($usage, $number, $start) = unpack("NNN", $indexentry);
-                    printf STDOUT ("          %06x: %s %d\n",
-                         $start, $usagestring, $number);
-                }
-            }
-        }
-    }
+	# glulx executable: look into its magic insides
+	if($type eq "GLUL") {
+		my ($major, $minor, $minimus) = (unpack("xxxxnCC", $chunkdata))[0,1,2];
+		print "\tGlulx version $major.$minor.$minimus\n";
+	}
+
+	# game identifier chunk: probably only if no executable chunk
+	if ($type eq "IFhd") {   
+	    my $release = unpack("n", substr($chunkdata,0,3));
+	    my $serialcode = substr($chunkdata, 2, 6);
+	    print "\t$release.$serialcode\n";
+	}
+
+	# release number chunk: zcode games only
+	if ($type eq "RelN") {
+		my $relnum = unpack("n", $chunkdata);
+	    print "\tRelease number $relnum\n";
+	}
+
+	# image chunk
+	if($type eq "PNG " or $type eq "JPEG") {
+		next unless $dump_images;
+		my $errstr = sprintf("No resource information for image at %0x06x\n", $pos);
+		my $filename = $images{$pos} . ($type eq "PNG " ? ".png" : ".jpg")
+			or warn($errstr), next;
+		open IMAGEFH, ">$filename"
+			or warn "Failed to open handle for $filename: $!\n", next;
+		binmode IMAGEFH;
+		local $\ = undef;
+		print IMAGEFH $chunkdata;
+		close IMAGEFH
+			or warn "Failed to close handle for $filename: $!\n", next;
+	}
+
+	# resource index chunk: always present
+	if ($type eq "RIdx") {
+		print "\tResources index:\n";
+
+		my $numres = unpack("N", $chunkdata);
+		substr($chunkdata, 0, 4) = "";
+		while($numres--) {
+			my($usage, $number, $start) =
+				unpack("a4 NN NN", substr($chunkdata, 0, 12, ""));
+			printf("\t\t%06x: %s %d\n", $start, $usage, $number);
+			$images{$start} = $number if $usage eq "Pict";
+		}
+	}
 }
 
 close(BLORB);
